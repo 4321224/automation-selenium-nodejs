4c6c6cdd33195a0848cbc80ad73cd043a5bf9cae
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-13 13:20:26 +0700

    sample automation test

---
+++ sampleTest.js
@@ -0,0 +1,20 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+
+
+(async function example() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    try {
+      await driver.manage().setTimeouts({ implicit: 1000 });
+      await driver.get("http://www.google.com");
+      // await driver.manage().window().fullscreen()
+
+      await driver
+        .findElement(By.xpath("//button/div[contains(text(), 'Accept all')]"))
+        .click();
+    //   const search_box = driver.findElement(By.name("q"));
+    //   search_box.sendKeys("webdriver", Key.ENTER);
+    //   await driver.wait(until.titleIs("webdriver - Google Seach"), 1000);
+    } finally {
+    //   await driver.quit();
+    }
+  })();
